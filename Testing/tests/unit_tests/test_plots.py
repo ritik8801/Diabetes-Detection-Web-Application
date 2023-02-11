37e8bc94e7d87bd9ee6525e62d0e0559acdbf2e6
Ritik Mishra <120116600+ritik8801@users.noreply.github.com>
2023-02-11 16:20:00 +0100

    Update test_plots.py

--- Testing/tests/unit_tests/test_plots.py
+++ Testing/tests/unit_tests/test_plots.py
@@ -22,9 +22,7 @@ def test_heatmap_observation(mocker):
     path = "https://raw.githubusercontent.com/ritik8801/Diabetes-Detection-Web-Application/main/Testing/tests/unit_tests/test_data/heatmap.png"
     data = pd.read_csv("https://raw.githubusercontent.com/ritik8801/Diabetes-Detection-Web-Application/main/Testing/tests/unit_tests/test_data/sample.csv")
     p = Plot(data, path)
-    try:
-        assert os.path.isfile("https://raw.githubusercontent.com/ritik8801/Diabetes-Detection-Web-Application/main/Testing/tests/unit_tests/test_data/heatmap.png")
-    except FileNotFoundError:
-        print("File not found, please verify the path")
+    assert os.path.isfile("https://raw.githubusercontent.com/ritik8801/Diabetes-Detection-Web-Application/main/Testing/tests/unit_tests/test_data/heatmap.png")
+    
 
     
