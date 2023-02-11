67008fd5300950d440f640ad1f22e96627fe5b26
Ritik Mishra <120116600+ritik8801@users.noreply.github.com>
2023-02-11 15:15:41 +0100

    Update test_plots.py

--- Testing/tests/unit_tests/test_plots.py
+++ Testing/tests/unit_tests/test_plots.py
@@ -12,15 +12,9 @@ def test_hist_observation(mocker):
     path = "https://raw.githubusercontent.com/ritik8801/Diabetes-Detection-Web-Application/main/Testing/tests/unit_tests/test_data/"
     data = pd.read_csv("https://raw.githubusercontent.com/ritik8801/Diabetes-Detection-Web-Application/main/Testing/tests/unit_tests/test_data/sample.csv")
     p = Plot(data, path)
-    path = "https://raw.githubusercontent.com/ritik8801/Diabetes-Detection-Web-Application/main/Testing/tests/unit_tests/test_data/histogram.png"
-    response = requests.get(path)
-    assert response.status_code == 200
 
 def test_heatmap_observation(mocker):
     mocker.patch("src.preprocessing.plots.Plot.heatmap_observation", return_value=None)
     path = "https://raw.githubusercontent.com/ritik8801/Diabetes-Detection-Web-Application/main/Testing/tests/unit_tests/test_data/"
     data = pd.read_csv("https://raw.githubusercontent.com/ritik8801/Diabetes-Detection-Web-Application/main/Testing/tests/unit_tests/test_data/sample.csv")
     p = Plot(data, path)
-    path = "https://raw.githubusercontent.com/ritik8801/Diabetes-Detection-Web-Application/main/Testing/tests/unit_tests/test_data/heatmap.png"
-    response = requests.get(path)
-    assert response.status_code == 200
