ae1e7cdd9706f93fa0d41f9da24037685e777064
Ritik Mishra <120116600+ritik8801@users.noreply.github.com>
2023-02-11 16:00:59 +0100

    Update test_plots.py

--- Testing/tests/unit_tests/test_plots.py
+++ Testing/tests/unit_tests/test_plots.py
@@ -9,7 +9,7 @@ from src.preprocessing.plots import Plot
 
 def test_hist_observation(mocker):
     mocker.patch("src.preprocessing.plots.Plot.hist_observation", return_value=None)
-    path = "https://raw.githubusercontent.com/ritik8801/Diabetes-Detection-Web-Application/main/Testing/tests/unit_tests/test_data/"
+    path = "https://raw.githubusercontent.com/ritik8801/Diabetes-Detection-Web-Application/main/Testing/tests/unit_tests/test_data"
     data = pd.read_csv("https://raw.githubusercontent.com/ritik8801/Diabetes-Detection-Web-Application/main/Testing/tests/unit_tests/test_data/sample.csv")
     p = Plot(data, path)
     plot_path = "https://raw.githubusercontent.com/ritik8801/Diabetes-Detection-Web-Application/main/Testing/tests/unit_tests/test_data/histogram.png"
@@ -23,7 +23,7 @@ def test_hist_observation(mocker):
 
 def test_heatmap_observation(mocker):
     mocker.patch("src.preprocessing.plots.Plot.heatmap_observation", return_value=None)
-    path = "https://raw.githubusercontent.com/ritik8801/Diabetes-Detection-Web-Application/main/Testing/tests/unit_tests/test_data/"
+    path = "https://raw.githubusercontent.com/ritik8801/Diabetes-Detection-Web-Application/main/Testing/tests/unit_tests/test_data"
     data = pd.read_csv("https://raw.githubusercontent.com/ritik8801/Diabetes-Detection-Web-Application/main/Testing/tests/unit_tests/test_data/sample.csv")
     p = Plot(data, path)
     plot_path = "https://raw.githubusercontent.com/ritik8801/Diabetes-Detection-Web-Application/main/Testing/tests/unit_tests/test_data/heatmap.png"
